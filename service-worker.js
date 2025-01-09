dde00ef3daf1d07afe78c15ae4d2cccf43200382
justaaaron <aaronhampson24@gmail.com>
2025-01-09 05:42:45 +0000

    zsdada

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/index.css',
-    '/qr-code.png',
-    '/student.png',
+    '/frick/',  // Correct start URL for your PWA
+    '/frick/index.html',
+    '/frick/index.css',
+    '/frick/qr-code.png',
+    '/frick/student.png',
     'https://code.jquery.com/jquery-3.6.0.min.js',
     'https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap',
     'https://fonts.gstatic.com'
@@ -21,11 +21,21 @@ self.addEventListener('install', event => {
 
 // Fetch event
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+    const requestUrl = new URL(event.request.url);
+    if (requestUrl.pathname.startsWith('/frick/')) {
+        event.respondWith(
+            caches.match(event.request).then(response => {
+                return response || fetch(event.request);
+            })
+        );
+    } else {
+        const modifiedUrl = new URL('/frick' + requestUrl.pathname, event.request.url);
+        event.respondWith(
+            caches.match(modifiedUrl).then(response => {
+                return response || fetch(modifiedUrl);
+            })
+        );
+    }
 });
 
 // Activate event
